fe9092d88d114c582e580151dac2a68984d09148
NikhilSeth <nikhilseth411@gmail.com>
2026-05-16 16:09:33 +0530

    initial commit

---
+++ src/main/java/com/bank/form121/Form121Application.java
@@ -0,0 +1,15 @@
+package com.bank.form121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Form121Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Form121Application.class, args);
+
+	}
+
+}
