fb732d9d0ee8244be2df1ea632bc9405c78ba821
NikhilSeth <nikhilseth411@gmail.com>
2026-05-20 14:24:47 +0530

    Connected frontend backend and MySQL

--- src/main/java/com/controller/CustomerController.java
+++ src/main/java/com/controller/CustomerController.java
@@ -3,12 +3,17 @@ package com.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.Customer;
 import com.service.CustomerService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CustomerController {
 
     @Autowired
