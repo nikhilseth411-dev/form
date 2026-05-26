8381a0c3f6d81d351b827e7bacf0e02f5e1df056
NikhilSeth <nikhilseth411@gmail.com>
2026-05-26 15:41:15 +0530

    secure index page only after login

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -1,37 +1,43 @@
 package com.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import dto.LoginRequest;
 
 @RestController
 @CrossOrigin("*")
+
 public class LoginController {
 
-@PostMapping("/login")
-public ResponseEntity<String> login(
-@RequestBody LoginRequest req
-){
+	@PostMapping("/login")
 
-if(
-req.getUsername().equals("admin")
-&&
-req.getPassword().equals("1234")
-){
+	public ResponseEntity<String> login(
 
-return ResponseEntity.ok(
-"Login Success");
+			@RequestBody LoginRequest req
 
-}
+	) {
 
-return ResponseEntity
-.status(401)
-.body("Invalid Credentials");
+		System.out.println("EMAIL = " + req.getEmail());
 
-}
+		System.out.println("PASSWORD = " + req.getPassword());
+
+		if (
+
+		"user@gmail.com".equals(req.getEmail())
+
+				&&
+
+				"bank123".equals(req.getPassword())
+
+		) {
+
+			return ResponseEntity.ok("Login Success");
+
+		}
+
+		return ResponseEntity.status(401).body("Invalid Credentials");
+
+	}
 
 }
--- src/main/java/dto/LoginRequest.java
+++ src/main/java/dto/LoginRequest.java
@@ -2,24 +2,24 @@ package dto;
 
 public class LoginRequest {
 
-private String username;
+	private String email;
 
-private String password;
+	private String password;
 
-public String getUsername() {
-return username;
-}
+	public String getEmail() {
+		return email;
+	}
 
-public void setUsername(String username) {
-this.username = username;
-}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-public String getPassword() {
-return password;
-}
+	public String getPassword() {
+		return password;
+	}
 
-public void setPassword(String password) {
-this.password = password;
-}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
